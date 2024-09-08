7bb3fdf1ad0496eba2752aeb2a60e4b722aa4006
felix <67316355+fxn-m@users.noreply.github.com>
2024-09-09 00:20:52 +0100

    remove dotenv config

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,5 @@
-import { config } from "dotenv"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
-config({ path: ".env" })
-
+console.log(process.env.DATABASE_URL)
 export const db = drizzle(postgres(process.env.DATABASE_URL!))
