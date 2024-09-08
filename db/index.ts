89b5f3aaced7680e837678f1e120b7cedada15ef
felix <67316355+fxn-m@users.noreply.github.com>
2024-09-09 00:25:15 +0100

    logging database connection

--- db/index.ts
+++ db/index.ts
@@ -2,4 +2,9 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
 console.log(process.env.DATABASE_URL)
+console.log("Connecting to database...")
+console.log("Connecting to database...")
+console.log("Connecting to database...")
+console.log("Connecting to database...")
+
 export const db = drizzle(postgres(process.env.DATABASE_URL!))
