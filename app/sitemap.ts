13c72fd15eed1dab65bad0e6624371e62e18d4e7
felix <67316355+fxn-m@users.noreply.github.com>
2024-09-20 03:18:33 +0100

    test with manual localised sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -44,12 +44,39 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       })
     })
 
+    // sitemapEntries.push({
+    //   url: `https://paulgraham-translated.vercel.app/essays`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 0.9,
+    //   alternates: generateAlternates("/essays")
+    // })
+
+    // sitemapEntries.push({
+    //   url: `https://paulgraham-translated.vercel.app/`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 1.0,
+    //   alternates: generateAlternates("")
+    // })
+
     sitemapEntries.push({
       url: `https://paulgraham-translated.vercel.app/essays`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
-      alternates: generateAlternates("/essays")
+      alternates: {
+        languages: {
+          en: "https://paulgraham-translated.vercel.app/essays/english",
+          fr: "https://paulgraham-translated.vercel.app/essays/french",
+          es: "https://paulgraham-translated.vercel.app/essays/spanish",
+          pt: "https://paulgraham-translated.vercel.app/essays/portuguese",
+          de: "https://paulgraham-translated.vercel.app/essays/german",
+          ja: "https://paulgraham-translated.vercel.app/essays/japanese",
+          hi: "https://paulgraham-translated.vercel.app/essays/hindi",
+          zh: "https://paulgraham-translated.vercel.app/essays/chinese"
+        }
+      }
     })
 
     sitemapEntries.push({
@@ -57,7 +84,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1.0,
-      alternates: generateAlternates("")
+      alternates: {
+        languages: {
+          en: "https://paulgraham-translated.vercel.app/?lang=english",
+          fr: "https://paulgraham-translated.vercel.app/?lang=french",
+          es: "https://paulgraham-translated.vercel.app/?lang=spanish",
+          pt: "https://paulgraham-translated.vercel.app/?lang=portuguese",
+          de: "https://paulgraham-translated.vercel.app/?lang=german",
+          ja: "https://paulgraham-translated.vercel.app/?lang=japanese",
+          hi: "https://paulgraham-translated.vercel.app/?lang=hindi",
+          zh: "https://paulgraham-translated.vercel.app/?lang=chinese"
+        }
+      }
     })
 
     return sitemapEntries
