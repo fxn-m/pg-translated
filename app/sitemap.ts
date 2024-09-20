65e4ea64b4e0e4d1ea7a9c246504a141936210d6
felix <67316355+fxn-m@users.noreply.github.com>
2024-09-20 03:19:59 +0100

    remove unused function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,9 @@
-import { languageCodes, supportedLanguages } from "@/db/schema"
-
 import { MetadataRoute } from "next"
 import { db } from "@/db"
 import { eq } from "drizzle-orm"
 import { essays } from "@/db/schema"
 import { models } from "@/db/schema"
-
-function generateAlternates(basePath: string) {
-  const alternates = {
-    languages: {} as Record<string, string>
-  }
-
-  for (const [languageName, code] of Object.entries(languageCodes)) {
-    alternates.languages[code] = `https://paulgraham-translated.vercel.app${basePath}${basePath ? "/" : "?lang="}${languageName}`
-  }
-
-  return alternates
-}
+import { supportedLanguages } from "@/db/schema"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
@@ -43,22 +30,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         })
       })
     })
-
-    // sitemapEntries.push({
-    //   url: `https://paulgraham-translated.vercel.app/essays`,
-    //   lastModified: new Date(),
-    //   changeFrequency: "weekly",
-    //   priority: 0.9,
-    //   alternates: generateAlternates("/essays")
-    // })
-
-    // sitemapEntries.push({
-    //   url: `https://paulgraham-translated.vercel.app/`,
-    //   lastModified: new Date(),
-    //   changeFrequency: "monthly",
-    //   priority: 1.0,
-    //   alternates: generateAlternates("")
-    // })
 
     sitemapEntries.push({
       url: `https://paulgraham-translated.vercel.app/essays`,
