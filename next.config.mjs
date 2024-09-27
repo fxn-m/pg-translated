aea77bd7f7ceab73991655f0330b8ed7e5a2dd72
felix <67316355+fxn-m@users.noreply.github.com>
2024-09-27 17:07:42 +0100

    Revert "remove ph badge"
    This reverts commit ad939bdca1c2bad305a5312c0b8b989fce243ecf.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.producthunt.com",
+        port: "",
+        pathname: "/widgets/embed-image/v1/**"
+      }
+    ]
+  }
+}
 
 export default nextConfig
